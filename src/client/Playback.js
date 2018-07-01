541b24b43702efeeab258e1aed7851a8151e4449
mameaw14 <pinyapornster@gmail.com>
2018-07-01 09:06:12 +0700

    Completed config React

---
+++ src/client/Playback.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './app.css';
+const THREE = require('three');
+
+export default class Playback extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+            Mameaw
+        </h1>
+      </div>
+    );
+  }
+}
